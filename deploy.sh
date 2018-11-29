c408b256d628572a38023b6d6a2423cf8608c02f
Jeremy Ottley <Gazaunga@gmail.com>
2018-11-29 12:57:51 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,8 @@ set -Eeuxo pipefail
 IFS=$'\n\t'
 
 trap 'echo Error at about $LINENO' ERR
+
+# Set Up Themes
+git clone --recursive https://github.com/lcpz/awesome-copycats.git
+mv -bv awesome-copycats/* ~/.config/awesome; rm -rf awesome-copycats
+
